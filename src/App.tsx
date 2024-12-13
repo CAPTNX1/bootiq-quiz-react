27d8a197935bc260dc3c23370c99203241c19a92
Filip Tichanek <tichanek.f@email.cz>
2024-12-13 17:16:39 +0100

    add tree

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import Landing from "./pages/Landing";
 import Teams from "./pages/Teams";
 import QuizBoard from "./components/QuizBoard";
+import Tree from "./assets/tree.jpeg";
 
 function App() {
   return (
     <div className="">
       <Header />
       <main>
+        <img src={Tree} width={900} className="tree" />
         <Router>
           <Switch>
             <Route path="/landing" component={Landing} />
